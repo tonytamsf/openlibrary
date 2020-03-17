07ac4ce4d96dd15e43b6ba25474ce289d517e0d6
cclauss <cclauss@me.com>
2020-03-17 11:25:46 +0100

    Use sorting to fix solr update_work

--- openlibrary/solr/update_work.py
+++ openlibrary/solr/update_work.py
@@ -509,7 +509,7 @@ class SolrProcessor:
                            for v in e[db_key])
             add_list(solr_key, values)
 
-        add_list("isbn", self.get_isbns(editions))
+        add_list("isbn", sorted(self.get_isbns(editions)))
         add("last_modified_i", self.get_last_modified(w, editions))
 
         self.add_ebook_info(d, editions)
@@ -637,11 +637,11 @@ class SolrProcessor:
 
         has_fulltext = any(e.get('ocaid', None) for e in editions)
 
-        add_list('ia', ia_list)
+        add_list('ia', sorted(ia_list))
         if has_fulltext:
             add('public_scan_b', public_scan)
         if all_collection:
-            add('ia_collection_s', ';'.join(all_collection))
+            add('ia_collection_s', ';'.join(sorted(all_collection)))
         if lending_edition:
             add('lending_edition_s', lending_edition)
             add('lending_identifier_s', lending_ia_identifier)
@@ -978,7 +978,7 @@ class UpdateRequest:
         node = dict2element(self.doc)
         root = Element("add")
         root.append(node)
-        return tostring(root).encode('utf-8')
+        return tostring(root, encoding="unicode")
 
     def tojson(self):
         """
@@ -1184,7 +1184,7 @@ def make_delete_query(keys):
     for key in keys:
         query = SubElement(delete_query,'query')
         query.text = 'key:%s' % key
-    return tostring(delete_query)
+    return tostring(delete_query, encoding="unicode")
 
 def update_author(akey, a=None, handle_redirects=True):
     """
