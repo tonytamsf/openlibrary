302d281a548b2317ba92a577d0b62fd49d1fc614
cclauss <cclauss@me.com>
2020-03-25 21:53:45 +0100

    dict --> list in test_marc.py

--- openlibrary/catalog/marc/tests/test_marc.py
+++ openlibrary/catalog/marc/tests/test_marc.py
@@ -123,8 +123,8 @@ class TestMarcParse(unittest.TestCase):
             output = subjects_for_work(MockRecord('650', value))
             if isinstance(expect, dict):
                 expect = list(expect)
-            if isinstance(output, dict):
-                output = list(output)
+            #if isinstance(output, dict):
+            #    output = list(output)
             self.assertEqual(list(expect), output)
 
     def test_read_title(self):
