1dd0d80435223ba6dbed2521afc41db91b4e8fb9
cclauss <cclauss@me.com>
2020-04-06 18:55:49 +0200

    Mandate more tests

--- scripts/test-py3.sh
+++ scripts/test-py3.sh
@@ -2,21 +2,6 @@
 
 pytest openlibrary/mocks openlibrary/olbase openlibrary/utils scripts/tests \
     openlibrary/catalog/add_book/tests/test_add_book.py \
-    openlibrary/coverstore/tests/test_code.py \
-    openlibrary/coverstore/tests/test_webapp.py \
-    openlibrary/plugins/admin/tests/test_services.py \
-    openlibrary/plugins/books/tests/test_doctests.py \
-    openlibrary/plugins/importapi/tests/test_code_ils.py \
-    openlibrary/plugins/importapi/tests/test_import_edition_builder.py \
-    openlibrary/plugins/openlibrary/tests/test_borrow_home.py \
-    openlibrary/plugins/openlibrary/tests/test_lists.py \
-    openlibrary/plugins/openlibrary/tests/test_stats.py \
-    openlibrary/plugins/upstream/tests/test_account.py \
-    openlibrary/plugins/upstream/tests/test_addbook.py \
-    openlibrary/plugins/upstream/tests/test_forms.py \
-    openlibrary/plugins/upstream/tests/test_related_carousels.py \
-    openlibrary/plugins/upstream/tests/test_utils.py \
-    openlibrary/plugins/worksearch/tests/test_worksearch.py \
     openlibrary/catalog/add_book/tests/test_load_book.py \
     openlibrary/catalog/add_book/tests/test_match.py \
     openlibrary/catalog/marc/tests/test_marc_binary.py \
@@ -25,7 +10,25 @@ pytest openlibrary/mocks openlibrary/olbase openlibrary/utils scripts/tests \
     openlibrary/catalog/merge/test_merge.py \
     openlibrary/catalog/merge/test_merge_marc.py \
     openlibrary/catalog/merge/test_names.py \
-    openlibrary/catalog/merge/test_normalize.py
+    openlibrary/catalog/merge/test_normalize.py \
+    openlibrary/coverstore/tests/test_code.py \
+    openlibrary/coverstore/tests/test_coverstore.py \
+    openlibrary/coverstore/tests/test_webapp.py \
+    openlibrary/plugins/admin/tests/test_services.py \
+    openlibrary/plugins/books/tests/test_doctests.py \
+    openlibrary/plugins/books/tests/test_dynlinks.py \
+    openlibrary/plugins/importapi/tests/test_code_ils.py \
+    openlibrary/plugins/importapi/tests/test_import_edition_builder.py \
+    openlibrary/plugins/openlibrary/tests/test_borrow_home.py \
+    openlibrary/plugins/openlibrary/tests/test_lists.py \
+    openlibrary/plugins/openlibrary/tests/test_stats.py \
+    openlibrary/plugins/upstream/tests/test_account.py \
+    openlibrary/plugins/upstream/tests/test_addbook.py \
+    openlibrary/plugins/upstream/tests/test_forms.py \
+    openlibrary/plugins/upstream/tests/test_merge_authors.py \
+    openlibrary/plugins/upstream/tests/test_related_carousels.py \
+    openlibrary/plugins/upstream/tests/test_utils.py \
+    openlibrary/plugins/worksearch/tests/test_worksearch.py
 
 # The following sections allow us to quickly spot tests that are fixed
 
@@ -37,10 +40,7 @@ pytest openlibrary/catalog/marc/tests/test_get_subjects.py \
     openlibrary/tests/catalog/test_utils.py || true
 
 # coverstore: All failing tests run in allow failures (|| true) mode
-pytest openlibrary/coverstore/tests/test_coverstore.py \
-    openlibrary/coverstore/tests/test_doctests.py || true
+pytest openlibrary/coverstore/tests/test_doctests.py || true
 
 # plugins: All failing tests run in allow failures (|| true) mode
-pytest openlibrary/plugins/books/tests/test_dynlinks.py \
-    openlibrary/plugins/openlibrary/tests/test_home.py \
-    openlibrary/plugins/upstream/tests/test_merge_authors.py || true
+pytest openlibrary/plugins/openlibrary/tests/test_home.py || true
